2b06164068a83df7d9f5dc39a9feea133d154214
Your Namedwadwadadfwadwadhawhjduwahdgit config --global user.email you@example.comgit config --global user.name Your <drohi2026@gmail.com>
2024-08-12 00:24:17 -0400

    FINAL CMAKE Setup

---
+++ build/CMakeFiles/project.exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project.exe.dir/main.cpp.o"
+  "CMakeFiles/project.exe.dir/main.cpp.o.d"
+  "project.exe"
+  "project.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
