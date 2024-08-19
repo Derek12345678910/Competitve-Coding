7d07d5cb5cb361bb15a2d086c43de351ebfb7741
Your Namedwadwadadfwadwadhawhjduwahdgit config --global user.email you@example.comgit config --global user.name Your <drohi2026@gmail.com>
2024-08-19 12:56:43 -0400

    small

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include "main.h"
 
 using namespace std;
 
+string type = "leetcode";
+
 int main(){
 
-    cout << "hi";
-
+    // Run specific file of choosing
+    if (type == "leetcode"){
+        // Leetcode file
+    }
+    else if (type == "comp"){
+        // Comp file
+    }
     return 0;
 }
