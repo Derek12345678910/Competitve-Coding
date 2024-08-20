10d494cf05db31ce49a5029b354915681346bbfa
Your Namedwadwadadfwadwadhawhjduwahdgit config --global user.email you@example.comgit config --global user.name Your <drohi2026@gmail.com>
2024-08-20 19:11:50 -0400

    Waterloo S1 2024 15/15

--- comp/S12024.cpp
+++ comp/S12024.cpp
@@ -1,11 +1,14 @@
-#include "../comp.h"
+// #include "../comp.h"
+
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 int S12024() {
     long N;
 
-    cout << "The number of total people: ";
+    //cout << "The number of total people: ";
     cin >> N;
 
     int matches = 0;
@@ -17,15 +20,22 @@ int S12024() {
 
     // ask for hats
     for (int i = 0; i < N; i++) {
-        cout << "Hat for person " << i << ": ";
+        //cout << "Hat for person " << i << ": ";
         cin >> tableSeats[i];
     }
 
     // check matches
     for (int i = 0; i < diameter; i++) {
         if (tableSeats[i] == tableSeats[i + diameter]) {
-            matches += 1;
+            matches += 2;
         }
     }
     return matches;
 }
+
+/* NEEDED FOR COMP SUBMISSION
+int main (){
+    cout << S12024();
+    return 0;
+}
+*/
